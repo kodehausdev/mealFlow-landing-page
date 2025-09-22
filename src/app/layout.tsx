61a73e8bd5cd5154247f8336a8c0f2380c08482b
Seyi Fatoki <fatokifury@gmail.com>
2025-09-22 22:29:21 +0100

    chore: add Vercel SpeedInsights to main layout for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: 'MealFlow: Simplify Your Household Meal Planning',
@@ -34,6 +36,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
